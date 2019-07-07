6089ab970e88dec0985454d3568a5b9720c8aa03
Katsumasa Kitajima <k.k4.bky@gmail.com>
2019-07-07 21:30:35 +0900

    Added a script to uninstall the program

---
+++ uninstall_ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Removing /usr/local/bin/image_viewer"
+sudo rm /usr/local/bin/image_viewer
+
+echo "Removing /usr/share/applications/image_viewer.desktop"
+sudo rm /usr/share/applications/image_viewer.desktop
+ 
